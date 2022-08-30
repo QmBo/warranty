646d8b7ff743d8c7205ead6cd0f29a08ebea3ca8
Victor Egorov <qrioflat@gmail.com>
2022-08-30 11:33:39 +0300

    Init

---
+++ src/main/java/ru/qmbo/warranty/WarrantyApplication.java
@@ -0,0 +1,13 @@
+package ru.qmbo.warranty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrantyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/qmbo/warranty/WarrantyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qmbo.warranty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
